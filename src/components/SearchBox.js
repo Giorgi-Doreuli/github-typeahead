8ab98d8e3ea044668edffbf24b150ddff33780aa
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-20 20:11:40 +0400

    created components for searchbox

---
+++ src/components/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SearchBox..css'
+
+function SearchBox() {
+  return (
+    <div>
+        <h2>hello</h2>
+        <input> hi</input>
+    </div>
+  )
+}
+
+export default SearchBox
