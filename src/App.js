c24693d30ad3f83b63f3d20f8d1552a01ab6483f
giorgi <giorgi.doreuli3721@gmail.com>
2022-02-20 20:11:05 +0400

    rendered new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchBox from './components/SearchBox';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <SearchBox />
     </div>
   );
 }
